f3f7791ed8866545d31c4cc323b825c014451887
Chris Matteson <chris.matteson@puppetlabs.com>
2015-12-01 06:01:13 +0000

    added confine statement to fact

--- lib/facter/services.rb
+++ lib/facter/services.rb
@@ -1,6 +1,7 @@
 # services.rb
 
 Facter.add(:services) do
+  confine :kernel => 'Linux'
   setcode do
     services = Facter::Core::Execution.exec('service --status-all | sed "s/^........//" | tr "\n" ","')
     services_array = services.split(',')
